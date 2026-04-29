179b26bcd82223f728ca8c4a2a2d64ff88a16c43
Marcel <marcel.grieb+neuefische@gmail.com>
2026-04-29 13:37:27 +0200

    added favorites to index

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,34 @@ const htmlForms = document.getElementsByTagName("form") as HTMLCollectionOf<HTML
 const searchForm = htmlForms[0] as HTMLFormElement;
 const searchBar = document.getElementById("search") as HTMLInputElement;
 const filterSelect = document.getElementById("by-publisher") as HTMLSelectElement;
-// const favorites_key = "favorites";
+const favoritesCount = document.getElementsByClassName("mainnav-number")[0] as HTMLElement;
+const favorites_key = "favorites";
+
+const isFavoriteBtn = `<svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="fav"
+                    >
+                      <path
+                        d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
+                      />
+                    </svg>`;
+
+const isNotFavoriteBtn = `<svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="fav"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                      />
+                    </svg>`
 
 async function fetchBooks(): Promise<Book[]> {
     const response = await fetch(apiUrl + "books");
@@ -52,46 +79,65 @@ function createBookRow(book: Book): HTMLTableRowElement {
 }
 
 function createFavBtn(book: Book): HTMLButtonElement {
+    const isFavorite = isBookFavorite(book);
     const favBtn = document.createElement("button");
     
     favBtn.className = "button button-clear fav-btn";
-    const svgNS = "http://www.w3.org/2000/svg";
-    const svg: SVGSVGElement = document.createElementNS(svgNS, "svg");
-    
-    svg.setAttribute("class", "fav");
-    svg.setAttribute("viewBox", "0 0 24 24");
-    svg.setAttribute("fill", "none");
-    svg.setAttribute("stroke-width", "1.5");
-    svg.setAttribute("stroke", "currentColor");
-
-    const svgPath: SVGPathElement = document.createElementNS(svgNS, "path");
-    svgPath.setAttribute("stroke-linecap", "round");
-    svgPath.setAttribute("stroke-linejoin", "round")
-    svgPath.setAttribute("d", "M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z");
-
-    svg.append(svgPath);
-    favBtn.append(svg);
+    if (isFavorite) {
+        favBtn.innerHTML = isFavoriteBtn;
+    } else {
+        favBtn.innerHTML = isNotFavoriteBtn;
+    }
 
     favBtn.addEventListener("click", () => {
-        console.log(book);
-        saveFavorite(book); // todo: implement
+        if (isFavorite) {
+            deleteFavorite(book);
+            favBtn.innerHTML = isNotFavoriteBtn;
+        } else {
+            addFavorite(book);
+            favBtn.innerHTML = isFavoriteBtn;
+        }
     });
 
     return favBtn;
 }
 
-function saveFavorite(book: Book): void {
+function isBookFavorite(book: Book): boolean {
+    const favorites: string[] = getFavorites();
+    const isFavorite: boolean = favorites.includes(book.isbn) ? true : false;
 
+    return isFavorite;
+}
 
+function addFavorite(book: Book): void {
+    const favorites = getFavorites();
 
-    console.log(book);
+    if (!favorites.includes(book.isbn)) {
+        favorites.push(book.isbn);
+        localStorage.setItem(favorites_key, JSON.stringify(favorites));
+    }
+}
+
+function deleteFavorite(book: Book): void {
+    //remove isbn from local storage
+}
+
+function getFavorites(): string[] {
+  const data = localStorage.getItem(favorites_key);
+
+  if (!data) return [];
+
+  try {
+    const isbns = JSON.parse(data);
+    return Array.isArray(isbns) ? isbns : [];
+  } catch {
+    return [];
+  }
 }
 
 function getFavoritesCount(): number {
-
-
-
-    return 5;
+    const favorites = getFavorites();
+    return favorites.length;
 }
 
 function createDetailBtn(book: Book): HTMLAnchorElement {
@@ -151,6 +197,7 @@ async function findBook(searchTerm: string): Promise<Book[]> {
 }
 
 const bookListData: Book[] = await fetchBooks();
+favoritesCount.innerText = getFavoritesCount().toString();
 
 bookList.innerHTML = "";
 
